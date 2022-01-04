100bdcb6a4a07d650c630a3971b52cbb48d2bce6
Angus Bezzina <angus@33bondi.com>
2022-01-04 14:21:38 -0600

    style: styling tweaks and responsiveness changes

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,11 +1,34 @@
+import React, { useEffect, useState } from "react";
+
 import Link from "next/link";
 
 import Logo from "components/logo";
 import Nav from "components/nav";
 
 const Header = () => {
+  const [active, setActive] = useState(false);
+  const toggleActive = () => {
+    const scrollDistance = 300;
+    const scrolled = window.scrollY;
+
+    if (scrolled > scrollDistance) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => toggleActive());
+    return document.removeEventListener("scroll", () => toggleActive());
+  }, []);
+
   return (
-    <header className="fixed top-0 right-0 w-full bg-primary text-white h-20 p-2 md:py-2 md:px-10 flex justify-between align-center shadow-lg z-50">
+    <header
+      className={`${
+        active ? "bg-primary text-white  shadow-lg" : "text-primary"
+      } fixed top-0 right-0 w-full h-20 p-2 md:py-2 md:px-10 flex justify-between align-center z-50`}
+    >
       <Link href="/">
         <a className="block" title="home">
           <Logo size="large" />
--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -13,7 +13,7 @@ const Hero = ({ title, subtitle, video, scrollButton }: HeroProps) => {
   return (
     <div className="relative h-96 md:h-screen w-full white-shade">
       <Container classNames="p-2 md:py-2 md:px-10 relative z-10 text-white">
-        <h1 className="feature-title">{title}</h1>
+        <h1 className="mt-12 md:mt-0 feature-title">{title}</h1>
         <h5 className="feature-subtitle">{subtitle}</h5>
       </Container>
       {video && (
