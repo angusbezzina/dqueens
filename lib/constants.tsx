70f3fb30c5f72c801289ca8f545098e3b4d3c94e
Angus Bezzina <angus@33bondi.com>
2022-02-02 12:49:13 -0600

    fix: map coordinate update

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -1,6 +1,6 @@
 export const googleApiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY ?? "";
 
-export const dqueensLocation = { lat: 20.59377, lng: -100.39746 };
+export const dqueensLocation = { lat: 20.6090824, lng: -100.3865437 };
 
 export const STRAPI_API_URL = process.env.NEXT_PUBLIC_STRAPI_API_PROD;
     
